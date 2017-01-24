47f4ca0d9ce3f19677fee128ebfeed40b0eb3180
Jony <jonysy@users.noreply.github.com>
2017-01-24 05:15:09 -0500

    Prepare

--- src/frameworks/native/stable_alloc.rs
+++
@@ -1,6 +0,0 @@
-/// Traditional allocation via creation of a filled Vec.
-pub fn allocate_boxed_slice(cap: usize) -> Box<[u8]> {
-    let vec: Vec<u8> = vec![0; cap];
-    let bx: Box<[u8]> = vec.into_boxed_slice();
-    bx
-}
