7bcb7ad30eea1923e0fc47711de9d91d468e3027
jonysy <jonysy@users.noreply.github.com>
2017-02-04 07:17:58 -0500

    Update lib.rs

--- frameworks/native/tests/lib.rs
+++ frameworks/native/tests/lib.rs
@@ -19,7 +19,7 @@ mod framework_spec {
 	use parenchyma_native::Native;
 
 	#[test]
-	fn it_can_get_native_device() {
+	fn it_can_init_native_framework() {
 		let framework = Native::new();
 		assert_eq!(framework.devices().len(), 1);
 	}
